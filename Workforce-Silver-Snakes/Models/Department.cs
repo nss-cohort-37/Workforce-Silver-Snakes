c2bd6eeeb966e235bbf95f45a8588cae1740de87
jamesnitz <jamesanitz2@gmail.com>
2020-04-14 13:55:38 -0500

    models createdgit add --allgit add --allgit add --all!

---
+++ Workforce-Silver-Snakes/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Workforce_Silver_Snakes.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Budget { get; set; }
+
+    }
+}
