b604d5075e6b54700b39b1866cf9fecadd1f2f49
Kevin Penny <Kevin.penny3@yahoo.com>
2020-04-14 14:28:31 -0500

    departments view is functioning

---
+++ Workforce-Silver-Snakes/Controllers/DepartmentsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Workforce_Silver_Snakes.Models.ViewModels;
+
+namespace Workforce_Silver_Snakes.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        private readonly IConfiguration _config;
+        public DepartmentsController(IConfiguration config)
+        {
+            _config = config;
+        }
+        //COMPUTED PROPERTY FOR THE CONNECTION
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+        // GET: Departments
+        public ActionResult Index()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT d.Id, d.[Name], d.Budget, COUNT(e.DepartmentId) as 'Employees'
+                                        FROM Department d
+                                        LEFT JOIN Employee e
+                                        ON	e.DepartmentId = d.Id
+                                        GROUP BY d.Name, d.Id, d.Budget ";
+                    var reader = cmd.ExecuteReader();
+                    var departments = new List<DepartmentsViewModel>();
+
+                    while (reader.Read())
+                    {
+                        departments.Add(new DepartmentsViewModel()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Budget = reader.GetInt32(reader.GetOrdinal("Budget")),
+                            Employees = reader.GetInt32(reader.GetOrdinal("Employees"))
+                        }
+                        );
+                    }
+                    reader.Close();
+                    return View(departments);
+                }
+            }
+        }
+
+        // GET: Departments/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Departments/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Departments/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Departments/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Departments/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Departments/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Departments/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
